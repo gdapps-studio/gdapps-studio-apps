31902fdbe6f4c72250085ee85e5cd9e76edb0bc2
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-04-13 07:03:31 +0200

    refactor: renamed packages
    - refactor: shortened to minimalistic + removed assets

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@gdapps-studio/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
