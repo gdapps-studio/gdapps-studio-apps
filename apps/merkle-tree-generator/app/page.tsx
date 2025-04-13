16e57c5d5d2c650a0130626df82296f5c4326555
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-04-13 10:07:15 +0200

    test: tailwindcss prod

--- apps/merkle-tree-generator/app/page.tsx
+++ apps/merkle-tree-generator/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@gdapps-studio/ui/button";
 export default function Home() {
   return (
     <div>
+      <h1 className="bg-red-200">tailwindcss</h1>
       <Button>Home</Button>
     </div>
   );
