575d0856559da9b226ef98cfddf5711d01f00ddb
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-04-26 19:41:51 +0200

    refactor: removed unused code and cleanup comments

--- apps/landing-page/src/app/layout.tsx
+++ apps/landing-page/src/app/layout.tsx
@@ -1,50 +1,7 @@
 import { Header } from "@/app/_/header";
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
 import { Footer } from "./_/footer";
 import "./globals.css";
-
-// const oxaniumFont = localFont({
-//   variable: "--font-oxanium",
-//   src: [
-//     {
-//       path: "~@gdapps-studio/assets/fonts/Oxanium-Regular.ttf",
-//       weight: "400",
-//       style: "normal",
-//     },
-//     {
-//       path: "~@gdapps-studio/assets/fonts/Oxanium-SemiBold.ttf",
-//       weight: "600",
-//       style: "semibold",
-//     },
-//     {
-//       path: "~@gdapps-studio/assets/fonts/Oxanium-Bold.ttf",
-//       weight: "700",
-//       style: "bold",
-//     },
-//   ],
-// });
-
-// const poppinsFont = localFont({
-//   variable: "--font-poppins",
-//   src: [
-//     {
-//       path: "~@gdapps-studio/assets/fonts/Poppins-Regular.ttf",
-//       weight: "400",
-//       style: "normal",
-//     },
-//     {
-//       path: "~@gdapps-studio/assets/fonts/Poppins-SemiBold.ttf",
-//       weight: "600",
-//       style: "semibold",
-//     },
-//     {
-//       path: "~@gdapps-studio/assets/fonts/Poppins-Bold.ttf",
-//       weight: "700",
-//       style: "bold",
-//     },
-//   ],
-// });
 
 export const metadata: Metadata = {
   title: "GDapps Studio",
@@ -59,10 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body
-        className={`antialiased`}
-        // className={`antialiased ${oxaniumFont.variable} ${poppinsFont.variable}`}
-      >
+      <body className={`antialiased`}>
         <Header />
         {children}
         <Footer />
