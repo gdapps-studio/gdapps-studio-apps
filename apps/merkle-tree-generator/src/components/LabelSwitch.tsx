4b24e821c3a0fa5f89a78dba5b4cbd1e331da8ed
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-04-26 19:54:21 +0200

    fix: format prettier

--- apps/merkle-tree-generator/src/components/LabelSwitch.tsx
+++ apps/merkle-tree-generator/src/components/LabelSwitch.tsx
@@ -1,14 +1,13 @@
-import { Switch } from './ui/switch'
-import { Label } from './ui/label'
-import * as SwitchPrimitive from '@radix-ui/react-switch'
-
+import { Switch } from "./ui/switch";
+import * as SwitchPrimitive from "@radix-ui/react-switch";
+import { Label } from "@gdapps-studio/ui/label";
 export const LabelSwitch = ({
-    label,
-    id,
-    ...rest
+  label,
+  id,
+  ...rest
 }: React.ComponentProps<typeof SwitchPrimitive.Root> & { label: string }) => (
-    <div className="flex items-center space-x-2">
-        <Switch id={id} {...rest} />
-        <Label htmlFor={id}>{label}</Label>
-    </div>
-)
+  <div className="flex items-center space-x-2">
+    <Switch id={id} {...rest} />
+    <Label htmlFor={id}>{label}</Label>
+  </div>
+);
