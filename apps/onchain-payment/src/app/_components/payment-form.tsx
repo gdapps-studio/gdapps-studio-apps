22dac7756a45acb8e610d3e18f8a983679158410
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-04-27 07:42:48 +0200

    feat: wrapped payment form with card and removed redundant form event subscriber

--- apps/onchain-payment/src/app/_components/payment-form.tsx
+++ apps/onchain-payment/src/app/_components/payment-form.tsx
@@ -34,6 +34,12 @@ import {
 } from "@/constants";
 import { ReactNode } from "react";
 import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@gdapps-studio/ui/card";
 
 const FormItemLabelAndDescription = ({
   input,
@@ -63,139 +69,152 @@ export const PaymentForm = ({
     defaultValues: paymentFormDefaultValues,
   });
 
-  const chain = form.watch("chain");
+  const chain = form.getValues("chain");
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit((values) => onSubmit?.(values))}
-        className="w-full max-w-xl space-y-6"
-      >
-        <FormField
-          control={form.control}
-          name="amount"
-          render={({ field }) => (
-            <FormItemLabelAndDescription
-              input={<Input placeholder={"0"} {...field} />}
-              description="Enter how much you want to receive"
-              label="Amount"
+    <Card
+      className="flex flex-col gap-6 w-full max-w-xl mx-auto px-8 py-6"
+      scaleOnHover={false}
+    >
+      <CardHeader>
+        <CardTitle>Create a payment</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit((values) => onSubmit?.(values))}
+            className="w-full max-w-xl space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItemLabelAndDescription
+                  input={<Input placeholder={"0"} {...field} />}
+                  description="Enter how much you want to receive"
+                  label="Amount"
+                />
+              )}
             />
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="address"
-          render={({ field }) => (
-            <FormItemLabelAndDescription
-              input={
-                <Input placeholder={chainToPlaceholder[chain]} {...field} />
-              }
-              description="Enter who is going to pay you"
-              label="Payer"
+            <FormField
+              control={form.control}
+              name="address"
+              render={({ field }) => (
+                <FormItemLabelAndDescription
+                  input={
+                    <Input placeholder={chainToPlaceholder[chain]} {...field} />
+                  }
+                  description="Enter who is going to pay you"
+                  label="Payer"
+                />
+              )}
             />
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="chain"
-          render={({ field }) => (
-            <FormItem className="w-full">
-              <FormLabel>Chain</FormLabel>
-              <Select
-                onValueChange={(value: ChainUnion) => {
-                  form.setValue("currency", chainToDefaultCurrency[value]);
-                  field.onChange(value);
-                }}
-                defaultValue={field.value}
-              >
-                <FormControl className="w-full">
-                  <SelectTrigger>
-                    <div className="flex items-center gap-1">
-                      <Image
-                        src={chainToImageSrc[field.value]}
-                        width={24}
-                        height={24}
-                        alt={`${field.value} chain logo`}
-                      />
-                      <span className="capitalize">{field.value}</span>
-                    </div>
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  {supportedChains.map((value) => (
-                    <SelectItem
-                      key={value}
-                      value={value}
-                      className="flex items-center"
+            <FormField
+              control={form.control}
+              name="chain"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Chain</FormLabel>
+                  <Select
+                    onValueChange={(value: ChainUnion) => {
+                      form.setValue("currency", chainToDefaultCurrency[value]);
+                      field.onChange(value);
+                    }}
+                    defaultValue={field.value}
+                  >
+                    <FormControl className="w-full">
+                      <SelectTrigger>
+                        <div className="flex items-center gap-1">
+                          <Image
+                            src={chainToImageSrc[field.value]}
+                            width={24}
+                            height={24}
+                            alt={`${field.value} chain logo`}
+                          />
+                          <span className="capitalize">{field.value}</span>
+                        </div>
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {supportedChains.map((value) => (
+                        <SelectItem
+                          key={value}
+                          value={value}
+                          className="flex items-center"
+                        >
+                          <Image
+                            src={chainToImageSrc[value]}
+                            width={24}
+                            height={24}
+                            alt={`${value} chain logo`}
+                          />
+                          <span className="capitalize">{value}</span>
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="currency"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Currency</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl className="w-full">
+                      <SelectTrigger>
+                        <div className="flex items-center gap-1">
+                          <Image
+                            src={currencyToImageSrc[field.value]}
+                            width={24}
+                            height={24}
+                            alt={`${field.value} currency logo`}
+                          />
+                          <span className="uppercase">{field.value}</span>
+                        </div>
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent
+                      defaultValue={chain === "solana" ? "sol" : "eth"}
                     >
-                      <Image
-                        src={chainToImageSrc[value]}
-                        width={24}
-                        height={24}
-                        alt={`${value} chain logo`}
-                      />
-                      <span className="capitalize">{value}</span>
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="currency"
-          render={({ field }) => (
-            <FormItem className="w-full">
-              <FormLabel>Currency</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl className="w-full">
-                  <SelectTrigger>
-                    <div className="flex items-center gap-1">
-                      <Image
-                        src={currencyToImageSrc[field.value]}
-                        width={24}
-                        height={24}
-                        alt={`${field.value} currency logo`}
-                      />
-                      <span className="uppercase">{field.value}</span>
-                    </div>
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent
-                  defaultValue={chain === "solana" ? "sol" : "eth"}
-                >
-                  {(chain === "solana"
-                    ? solCurrencyValues
-                    : ethCurrencyValues
-                  ).map((currency) => (
-                    <SelectItem
-                      key={currency}
-                      className="uppercase"
-                      value={currency}
-                    >
-                      <div className="flex items-center gap-1">
-                        <Image
-                          src={currencyToImageSrc[currency]}
-                          width={24}
-                          height={24}
-                          alt={`${currency} currency logo`}
-                        />
-                        <span className="uppercase">{currency}</span>
-                      </div>
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button className="w-full" size={"lg"} type="submit">
-          Submit
-        </Button>
-      </form>
-    </Form>
+                      {(chain === "solana"
+                        ? solCurrencyValues
+                        : ethCurrencyValues
+                      ).map((currency) => (
+                        <SelectItem
+                          key={currency}
+                          className="uppercase"
+                          value={currency}
+                        >
+                          <div className="flex items-center gap-1">
+                            <Image
+                              src={currencyToImageSrc[currency]}
+                              width={24}
+                              height={24}
+                              alt={`${currency} currency logo`}
+                            />
+                            <span className="uppercase">{currency}</span>
+                          </div>
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="w-full" size={"lg"} type="submit">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
