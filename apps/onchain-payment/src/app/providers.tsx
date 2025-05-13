c92a95a9c1a5b2283049c2841a076621ef17fda3
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-05-13 10:22:48 +0200

    feat: wallet connect project id
    - refactor: removed unused

--- apps/onchain-payment/src/app/providers.tsx
+++ apps/onchain-payment/src/app/providers.tsx
@@ -11,7 +11,6 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import {
   RAINBOW_KIT_APP_NAME,
   RAINBOW_KIT_BASE_PRC_URL,
-  RAINBOW_KIT_PROJECT_ID,
   rainbowKitTheme,
 } from "@/constants";
 import {
@@ -28,7 +27,7 @@ import { Footer } from "./_components/footer";
 
 const config = getDefaultConfig({
   appName: RAINBOW_KIT_APP_NAME,
-  projectId: RAINBOW_KIT_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
   chains: [mainnet],
   transports: {
     [mainnet.id]: http(
@@ -42,17 +41,21 @@ const queryClient = new QueryClient();
 
 const QUICK_NODE_BASE_URL =
   "https://fluent-soft-violet.solana-mainnet.quiknode.pro";
-const ENDPOINT =
+const endpoint =
   process.env.NODE_ENV === "development"
     ? clusterApiUrl("devnet")
     : `${QUICK_NODE_BASE_URL}/${process.env.NEXT_PUBLIC_QUICKNODE_KEY}`;
 
-const connection = new Connection(ENDPOINT, "confirmed");
+const connection = new Connection(endpoint, "confirmed");
 
 export const Providers = ({ children }: { children: ReactNode }) => {
-  const wallets = useMemo(() => [new UnsafeBurnerWalletAdapter()], []);
+  const wallets = useMemo(
+    () => [new UnsafeBurnerWalletAdapter()],
+
+    [],
+  );
   return (
-    <ConnectionProvider endpoint={ENDPOINT}>
+    <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
           <WagmiProvider config={config}>
