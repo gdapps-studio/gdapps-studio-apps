a1a634ec1cd3fff09a8d98c68a2adc0287462061
0xCodeMaieutics <gio.shara12345@gmail.com>
2026-04-09 14:03:47 +0200

    fix: wording

--- apps/landing-page/src/app/_/hero/banner.tsx
+++ apps/landing-page/src/app/_/hero/banner.tsx
@@ -16,7 +16,7 @@ export const Banner = () => {
       className="absolute bottom-10 left-0 bg-zinc-950 text-white border-2 border-emerald-400 rounded-2xl font-semibold px-6 py-4 shadow-lg max-w-[300px]"
     >
       Work with us for better results in less time to unleash your full
-      potential quick
+      potential
     </animated.div>
   );
 };
